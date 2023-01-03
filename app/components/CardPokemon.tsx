a3aebe462daa051720438b27f50396486392ea2b
Geronimo Nahuel Giordano <nahuelgiordano@hotmail.com>
2023-01-03 15:03:26 -0300

    remove icon

--- app/components/CardPokemon.tsx
+++ app/components/CardPokemon.tsx
@@ -4,8 +4,6 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
-import { GiWaterDrop } from "react-icons/Gi";
-
 export default function CardPokemon({ pokemon }) {
   return (
     <Card
